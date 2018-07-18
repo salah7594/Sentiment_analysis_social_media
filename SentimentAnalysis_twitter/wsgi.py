5ebe4df6bf645a8a0850ce1b4a5222a7d2756e56
salah7594 <32416435+salah7594@users.noreply.github.com>
2018-07-18 20:19:39 +0300

    Update wsgi.py
    Resolve the issue : 
    $ heroku config:set DISABLE_COLLECTSTATIC=1

--- SentimentAnalysis_twitter/wsgi.py
+++ SentimentAnalysis_twitter/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SentimentAnalysis_twitter.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
